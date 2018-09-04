be470dc73e98206b0b0b80d0f8a8cfd3c9104b3a
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-09-04 17:13:39 -0600

    closer

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,11 +20,44 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    render partial: "form"
   end
 
   def create
+    @item = @department.items.new(item_params)
 
+    if @item.save
+      redirect_to [@department, @item]
+    else
+      render :new
+    end
   end
 
-  def 
+  def update
+    if @item.update(item_params)
+      redirect_to [@department, @item]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to department_items_path
+  end
+
+  private
+
+  def set_department
+    @department = Department.find(params[:department_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+  
+    params.require(:item).permit(:name, :description)
+  end
 end
